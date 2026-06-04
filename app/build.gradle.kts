93dce204410810c8d53cfe1985e9597772e61128
bobbidenisleo-glitch <bobbidenisleo@gmail.com>
2026-06-04 10:51:50 +0300

    Fix shadow plugin: use com.gradleup.shadow

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     id("java")
     id("jacoco")
     id("org.sonarqube") version "5.1.0.4882"
+    id("application")
+    id("com.gradleup.shadow") version "8.3.0"
 }
 
 group = "hexlet.code"
@@ -12,6 +14,16 @@ repositories {
 }
 
 dependencies {
+    // Javalin web framework
+    implementation("io.javalin:javalin:6.1.3")
+    
+    // Logger
+    implementation("org.slf4j:slf4j-simple:2.0.13")
+    
+    // Jackson for JSON handling (required by Javalin)
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.16.1")
+    
+    // Testing
     testImplementation(platform("org.junit:junit-bom:6.0.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
@@ -30,6 +42,10 @@ tasks.jacocoTestReport {
     }
 }
 
+application {
+    mainClass.set("hexlet.code.App")
+}
+
 sonar {
     properties {
         property("sonar.projectKey", "bobbidenisleo-glitch_java-project-72")
@@ -37,4 +53,9 @@ sonar {
         property("sonar.host.url", "https://sonarcloud.io")
         property("sonar.coverage.jacoco.xmlReportPaths", "build/reports/jacoco/test/jacocoTestReport.xml")
     }
+}
+
+tasks.shadowJar {
+    mergeServiceFiles()
+    archiveFileName.set("app-all.jar")
 }
